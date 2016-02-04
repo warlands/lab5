bacdd17959e267d9e3b6dbb44446e8f85995eb86
vagrant <vagrant@precise32.(none)>
2016-02-04 20:10:45 +0000

    made changes

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,6 @@ var data = require('../data.json');
 
 exports.view = function(req, res){
 	console.log(data);
-	res.render('index', {
-        'data'
-    });
+	res.render('index', 
+        data);
 };
